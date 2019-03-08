110648e9b3526e9c9dfd64f57c158f338659c0c7
Jed Mao <jedmao@outlook.com>
2019-03-08 13:15:32 -0600

    Introduce object-omit type definitions

---
+++ packages/object-omit/index.ts
@@ -0,0 +1,44 @@
+/**
+ * The opposite of `just-pick`; this method creates an object composed of the
+ * own and inherited enumerable properties of `object` that are not omitted.
+ *
+ * @category Object
+ * @returns A new object with specified keys omitted from the source `object`.
+ * @example
+```ts
+var obj = {a: 3, b: 5, c: 9};
+omit(obj, ['a', 'c']); // {b: 5}
+omit(obj, a, c); // {b: 5}
+omit(obj, ['a', 'b', 'd']); // {c: 9}
+omit(obj, ['a', 'a']); // {b: 5, c: 9}
+```
+ */
+function omit<T extends object, K extends keyof T>(
+  /**
+   * The source object.
+   */
+  obj: T | null | undefined,
+  /**
+   * The property names to omit.
+   */
+  remove: keyof T | Array<K | ReadonlyArray<K>>,
+  /**
+   * The property names to omit.
+   */
+  ...rest: Array<K | ReadonlyArray<K>>
+) {
+  var result = {} as any;
+  if (typeof remove === 'string') {
+    remove = [remove as any].concat(rest)
+  }
+  for (var prop in obj) {
+    if (!obj.hasOwnProperty || obj.hasOwnProperty(prop)) {
+      if ((remove as Array<K | ReadonlyArray<K>>).indexOf(prop as unknown as K) === -1) {
+        result[prop as string] = obj[prop];
+      }
+    }
+  }
+  return result;
+}
+
+export = omit;
